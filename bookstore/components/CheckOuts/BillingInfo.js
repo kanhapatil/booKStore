de592dcd7d7e265658dbe000a7135e8567182b89
unknown <kanhap569@gmail.com>
2024-05-28 09:01:26 +0530

    Working with billing information

--- bookstore/components/CheckOuts/BillingInfo.js
+++ bookstore/components/CheckOuts/BillingInfo.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import styles from "../../app/ShoppingCart/Checkout/[Checkout]/Checkout.module.css";
 
 const BillingInfo = () => {
+  const [address, setAddress] = useState();
+  useEffect(() => {
+    const fetchAddress = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (token) {
+          const response = await axios.get(
+            "http://127.0.0.1:8000/user/address/",
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          setAddress(response.data);
+        } else {
+          console.log("something went wrong");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAddress();
+  }, []);
+
+  if (address){
+    console.log(address[0]["state"]);
+  }
   return (
     <>
       <div className={styles.billingInfo}>
@@ -38,9 +67,10 @@ const BillingInfo = () => {
             <input
               type="text"
               className={styles.input}
+              value={address? address[0]["area"]: null}
               placeholder="Street Address"
             />
-            <input type="text" className={styles.input} placeholder="Country" />
+            <input type="text" className={styles.input} value="India" placeholder="Country" />
           </div>
 
           <p style={{ marginTop: "0.6rem" }}>
@@ -48,13 +78,13 @@ const BillingInfo = () => {
           </p>
 
           <div className={styles.address2}>
-            <input type="number" className={styles.input} placeholder="Zip" />
-            <input type="text" className={styles.input} placeholder="City" />
-            <input type="text" className={styles.input} placeholder="State" />
+            <input type="number" className={styles.input} value={address? address[0]["zipcode"]: null} placeholder="Zip" />
+            <input type="text" className={styles.input} value={address? address[0]["city"]: null} placeholder="City" />
+            <input type="text" className={styles.input} value={address? address[0]["state"]: null} placeholder="State" />
           </div>
 
           <div className={styles.addAddress}>
-            <button className={styles.button1}>Submit</button>
+            <button className={styles.button1}>Update</button>
           </div>
         </form>
       </div>
